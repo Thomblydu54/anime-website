1f332e2660d97803f097543a1ab74dfbd321ed77
Thomblydu54 <weillandthomas@gmail.com>
2026-07-05 13:56:28 +0000

    Ajout du déploiement GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwindcss from '@tailwindcss/vite';
+const isProd = process.env.NODE_ENV === "production";
 
-// https://astro.build/config
 export default defineConfig({
+
+  site: "https://thomblydu54.github.io",
+
+  base: isProd ? "/anime-website/" : "/",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
 });
