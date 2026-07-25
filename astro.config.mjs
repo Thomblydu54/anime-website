1991b954eea1bf3cf5cd46c13b25dfbc6012b2f4
Thomblydu54 <weillandthomas@gmail.com>
2026-07-25 13:21:18 +0000

    Description des modifications

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 const isProd = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  site: "https://thomblydu54.github.io",
+  site: "https://thomblydu54.github.io/anime-website/",
   base: "/anime-website/",
 
   vite: {
